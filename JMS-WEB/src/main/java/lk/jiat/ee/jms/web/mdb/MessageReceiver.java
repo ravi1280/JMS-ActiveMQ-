6a1e81a8a9af746458070462bf410d76789d2e3e
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-05-27 22:52:31 +0530

    update

--- JMS-WEB/src/main/java/lk/jiat/ee/jms/web/mdb/MessageReceiver.java
+++ JMS-WEB/src/main/java/lk/jiat/ee/jms/web/mdb/MessageReceiver.java
@@ -1,11 +1,17 @@
 package lk.jiat.ee.jms.web.mdb;
 
+import jakarta.ejb.ActivationConfigProperty;
 import jakarta.ejb.MessageDriven;
 import jakarta.jms.JMSException;
 import jakarta.jms.Message;
 import jakarta.jms.MessageListener;
 
-@MessageDriven(activationConfig = {})
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "jakarta.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/MyTopic"),
+//        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/MyQueue"),
+        @ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1"),
+})
 public class MessageReceiver implements MessageListener {
     @Override
     public void onMessage(Message message) {
