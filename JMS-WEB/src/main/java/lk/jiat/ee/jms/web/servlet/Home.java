e60ca9d89d6eb0e681d0ed1c529507cf7df44279
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-05-28 20:27:48 +0530

    Update

--- JMS-WEB/src/main/java/lk/jiat/ee/jms/web/servlet/Home.java
+++ JMS-WEB/src/main/java/lk/jiat/ee/jms/web/servlet/Home.java
@@ -15,34 +15,34 @@ import java.io.IOException;
 @WebServlet("/home")
 public class Home extends HttpServlet {
 
-    @Resource(lookup = "jms/MyQueueConnectionFactory")
-    private QueueConnectionFactory connectionFactory;
-
-    @Resource(lookup = "jms/MyQueue")
-    private Queue queue;
+//    @Resource(lookup = "jms/MyQueueConnectionFactory")
+//    private QueueConnectionFactory connectionFactory;
+//
+//    @Resource(lookup = "jms/MyQueue")
+//    private Queue queue;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("<h1>Home Page</h1>");
 
-        try {
-            QueueConnection connection = connectionFactory.createQueueConnection();
-            QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-
-            jakarta.jms.QueueSender sender = session.createSender(queue);
-
-            for (int i = 0; i < 50; i++) {
-                TextMessage message = session.createTextMessage();
-                message.setText("Hello World, " + i);
-                sender.send(message);
-            }
-
-            sender.close();
-            session.close();
-            connection.close();
-
-        } catch (JMSException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            QueueConnection connection = connectionFactory.createQueueConnection();
+//            QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+//
+//            jakarta.jms.QueueSender sender = session.createSender(queue);
+//
+//            for (int i = 0; i < 50; i++) {
+//                TextMessage message = session.createTextMessage();
+//                message.setText("Hello World, " + i);
+//                sender.send(message);
+//            }
+//
+//            sender.close();
+//            session.close();
+//            connection.close();
+//
+//        } catch (JMSException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
